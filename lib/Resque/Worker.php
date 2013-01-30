401f97e8d658114e6fbf210a25cdaa30eefe09ec
Kamisama <kami@kamisama.me>
2013-01-30 16:55:48 -0500

    Detect the autoloader class up to 3 directory level

--- lib/Resque/Worker.php
+++ lib/Resque/Worker.php
@@ -4,15 +4,26 @@ require_once dirname(__FILE__) . '/Event.php';
 require_once dirname(__FILE__) . '/Job.php';
 require_once dirname(__FILE__) . '/Job/DirtyExitException.php';
 
-// The library is the root library
-if (file_exists(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'))
-{
-	require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+// Find and initialize Composer
+$files = array(
+    __DIR__ . '/../../vendor/autoload.php',
+    __DIR__ . '/../../../../autoload.php',
+    __DIR__ . '/../vendor/autoload.php',
+);
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        require_once $file;
+        break;
+    }
 }
-// The library is a dependency of another library
-elseif (file_exists(dirname(dirname(dirname(dirname(__DIR__)))) . DIRECTORY_SEPARATOR . 'autoload.php'))
-{
-	require_once dirname(dirname(dirname(dirname(__DIR__)))) . DIRECTORY_SEPARATOR . 'autoload.php';
+
+if (!class_exists('Composer\Autoload\ClassLoader', false)) {
+    die(
+        'You need to set up the project dependencies using the following commands:' . PHP_EOL .
+        'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL
+    );
 }
 
 /**
