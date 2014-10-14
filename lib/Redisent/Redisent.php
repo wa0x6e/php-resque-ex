e5377f809b464a45efcd1bdf02bc381095437dcf
Matthieu Fauveau <mfauveau@gmail.com>
2014-10-14 15:45:28 -0400

    Proper fix for the formatArgument function in Redisent

--- lib/Redisent/Redisent.php
+++ lib/Redisent/Redisent.php
@@ -146,12 +146,7 @@ class Redisent {
 
     private function formatArgument($arg) {
         if (is_array($arg)) {
-            $len = 0;
-            foreach ($arg as $a) {
-                $len += strlen($a);
-            }
-        } else {
-            $len = strlen($arg);
+            $arg = implode(' ', $arg);
         }
         return sprintf('$%d%s%s', $len, CRLF, $arg);
     }
