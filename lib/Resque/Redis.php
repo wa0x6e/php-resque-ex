bbada2d937856f9a3cef088a95812d985acf7d57
hooman naghiee <hooman@rtler.com>
2015-07-11 12:42:09 +0430

    update redis key command list and add dequeue tests.

--- lib/Resque/Redis.php
+++ lib/Resque/Redis.php
@@ -1,7 +1,6 @@
 <?php
 
-if (class_exists('Redis'))
-{
+if (class_exists('Redis')) {
 	class RedisApi extends Redis
 	{
 		private static $defaultNamespace = 'resque:';
@@ -20,7 +19,7 @@ if (class_exists('Redis'))
 
 		function establishConnection()
 		{
-			$this->pconnect($this->host, (int) $this->port, (int) $this->timeout, getmypid());
+			$this->pconnect($this->host, (int)$this->port, (int)$this->timeout, getmypid());
 			if ($this->password !== null) {
 				$this->auth($this->password);
 			}
@@ -39,12 +38,10 @@ if (class_exists('Redis'))
 			$this->setOption(Redis::OPT_PREFIX, self::$defaultNamespace);
 		}
 	}
-}
-else
-{
+} else {
 	// Third- party apps may have already loaded Resident from elsewhere
 	// so lets be careful.
-	if(!class_exists('Redisent', false)) {
+	if (!class_exists('Redisent', false)) {
 		require_once dirname(__FILE__) . '/../Redisent/Redisent.php';
 	}
 
@@ -52,10 +49,10 @@ else
 	 * Extended Redisent class used by Resque for all communication with
 	 * redis. Essentially adds namespace support to Redisent.
 	 *
-	 * @package		Resque/Redis
-	 * @author		Chris Boulton <chris.boulton@interspire.com>
-	 * @copyright	(c) 2010 Chris Boulton
-	 * @license		http://www.opensource.org/licenses/mit-license.php
+	 * @package        Resque/Redis
+	 * @author        Chris Boulton <chris.boulton@interspire.com>
+	 * @copyright    (c) 2010 Chris Boulton
+	 * @license        http://www.opensource.org/licenses/mit-license.php
 	 */
 	class RedisApi extends Redisent
 	{
@@ -66,51 +63,52 @@ else
 		private static $defaultNamespace = 'resque:';
 		/**
 		 * @var array List of all commands in Redis that supply a key as their
-		 *	first argument. Used to prefix keys with the Resque namespace.
+		 *    first argument. Used to prefix keys with the Resque namespace.
 		 */
-		private $keyCommands = array(
-				'exists',
-				'del',
-				'type',
-				'keys',
-				'expire',
-				'ttl',
-				'move',
-				'set',
-				'get',
-				'getset',
-				'setnx',
-				'incr',
-				'incrby',
-				'decr',
-				'decrby',
-				'rpush',
-				'lpush',
-				'llen',
-				'lrange',
-				'ltrim',
-				'lindex',
-				'lset',
-				'lrem',
-				'lpop',
-				'rpop',
-				'sadd',
-				'srem',
-				'spop',
-				'scard',
-				'sismember',
-				'smembers',
-				'srandmember',
-				'zadd',
-				'zrem',
-				'zrange',
-				'zrevrange',
-				'zrangebyscore',
-				'zcard',
-				'zscore',
-				'zremrangebyscore',
-				'sort'
-		);
+		private $keyCommands = [
+			'exists',
+			'del',
+			'type',
+			'keys',
+			'expire',
+			'ttl',
+			'move',
+			'set',
+			'get',
+			'getset',
+			'setnx',
+			'incr',
+			'incrby',
+			'decr',
+			'decrby',
+			'rpush',
+			'lpush',
+			'llen',
+			'lrange',
+			'ltrim',
+			'lindex',
+			'lset',
+			'lrem',
+			'lpop',
+			'rpop',
+			'sadd',
+			'srem',
+			'spop',
+			'scard',
+			'sismember',
+			'smembers',
+			'srandmember',
+			'zadd',
+			'zrem',
+			'zrange',
+			'zrevrange',
+			'zrangebyscore',
+			'zcard',
+			'zscore',
+			'zremrangebyscore',
+			'sort',
+			'rpoplpush',
+		];
 		// sinterstore
 		// sunion
 		// sunionstore
@@ -119,7 +117,6 @@ else
 		// sinter
 		// smove
 		// rename
-		// rpoplpush
 		// mget
 		// msetnx
 		// mset
@@ -145,26 +142,27 @@ else
 		 * @param array $args Array of supplied arguments to the method.
 		 * @return mixed Return value from Resident::call() based on the command.
 		 */
-		public function __call($name, $args) {
+		public function __call($name, $args)
+		{
 			$args = func_get_args();
-			if(in_array($name, $this->keyCommands)) {
+			if (in_array($name, $this->keyCommands)) {
 				$args[1][0] = self::$defaultNamespace . $args[1][0];
 			}
 			try {
 				return parent::__call($name, $args[1]);
-			}
-			catch(RedisException $e) {
+			} catch (RedisException $e) {
 				return false;
 			}
 		}
 	}
 }
 
-class Resque_Redis extends redisApi {
+class Resque_Redis extends redisApi
+{
 
 	public function __construct($host, $port, $password = null)
 	{
-		if(is_subclass_of($this, 'Redis')){
+		if (is_subclass_of($this, 'Redis')) {
 			parent::__construct($host, $port, 5, $password);
 		} else {
 			parent::__construct($host, $port);
